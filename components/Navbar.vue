116234c0cb905ab6cd9a6db83c263ee13d46ce2f
snak1o <snakwcs@gmail.com>
2022-02-25 02:30:40 +0300

    Update navbar layout

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,20 +1,28 @@
 <template>
   <header class="header">
-    <div class="header-layout">
-      <div class="logo">Logo</div>
-      <div class="search"></div>
+    <div class="container">
+      <div class="header-layout">
+        <div class="logo"><nuxt-link to="/">Logo</nuxt-link></div>
+        <div class="search">
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+      <div class="header-layout">
+        <nav class="nav">
+          <ul class="nav-list">
+            <li class="nav-list-item"><nuxt-link to="/">Home</nuxt-link></li>
+            <li class="nav-list-item"><nuxt-link to="/">Boats</nuxt-link></li>
+            <li class="nav-list-item"><nuxt-link to="/">About us</nuxt-link></li>
+            <li class="nav-list-item"><nuxt-link to="/">Contact us</nuxt-link></li>
+            <li class="nav-list-item"><nuxt-link to="/">Profile</nuxt-link></li>
+            <li class="nav-list-item"><nuxt-link to="/">Cart</nuxt-link></li>
+          </ul>
+        </nav>
+        <div class="cart">
+          0$ 0 products
+        </div>
+      </div>
     </div>
-  <div class="header-layout">
-    <nav>
-      <li>Home</li>
-      <li>Boats</li>
-      <li>About us</li>
-      <li>Contact us</li>
-      <li>Profile</li>
-      <li>Cart</li>
-    </nav>
-    <div class="cart"></div>
-  </div>
   </header>
 </template>
 
@@ -25,5 +33,18 @@ export default {
 </script>
 
 <style scoped>
-
+  .header {
+    height: 250px;
+    border-bottom: 1px solid lightgrey;
+    display: flex;
+    flex-direction: column;
+  }
+  .header-layout {
+    display: flex;
+    justify-content: space-between;
+  }
+  .nav-list {
+    list-style: none;
+    display: flex;
+  }
 </style>
