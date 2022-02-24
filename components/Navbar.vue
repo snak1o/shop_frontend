69e590724939f3bfaa90071f8730d1728c427320
snak1o <snakwcs@gmail.com>
2022-02-24 22:25:44 +0300

    Add Navbar layout

---
+++ components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <div class="header-layout">
+      <div class="logo">Logo</div>
+      <div class="search"></div>
+    </div>
+  <div class="header-layout">
+    <nav>
+      <li>Home</li>
+      <li>Boats</li>
+      <li>About us</li>
+      <li>Contact us</li>
+      <li>Profile</li>
+      <li>Cart</li>
+    </nav>
+    <div class="cart"></div>
+  </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
