831b20329bceacdb01b44d97c8a488ba88e0faa6
snak1o <snakwcs@gmail.com>
2022-03-03 01:15:20 +0300

    Add components layout

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    <div class="container footer-container">
+     r-båtskydd &copy; 2022
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .footer {
+    width: 100%;
+    background: #f2f2f2;
+    padding: 30px 0;
+  }
+</style>
---
+++ components/StaticNotification.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="notification">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .notification {
+    border-left: 0.5em solid rgba(0,0,0,.15);
+    border-radius: 5px;
+    display: flex;
+    padding: 16px 32px 16px 50px;
+    color: white;
+    position: relative;
+  }
+  .info {
+    background: #3d9cd2;
+  }
+  .info:before {
+    left: 17.5px;
+    position: absolute;
+    content: "";
+    width: 20px;
+    height: 20px;
+    background: url("/info.svg");
+  }
+</style>
