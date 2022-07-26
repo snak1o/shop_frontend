5097da51ce2caf45736a8c9ce8d0394b10fbc485
snak1o <snakwcs@gmail.com>
2022-07-26 17:09:39 +0300

    Connect photo preview component

--- pages/boats/_id.vue
+++ pages/boats/_id.vue
@@ -1,8 +1,16 @@
 <template>
-  <section class="text-gray-600 body-font">
+  <section class="text-gray-600 body-font" :class="preview ? 'overflow-hidden' : ''">
     <div class="container px-5 py-24 mx-auto">
       <div class="lg:w-4/5 mx-auto flex flex-wrap">
-        <img alt="ecommerce" class="lg:w-1/2 w-full h-auto object-cover object-center rounded" :src="item.photos[0]">
+        <div class="lg:w-1/2 w-full h-auto select-none space-y-5">
+          <img alt="ecommerce" class="object-cover object-center cursor-pointer rounded" :src="activeImg" @click="preview = true" >
+          <div class="inline-grid grid-cols-5 gap-5 items-center">
+            <div v-for="img in item.photos" :key="img" class="h-24 cursor-pointer flex justify-center" @click="activeImg = img">
+              <img class=" object-cover object-center rounded" :src="img" :alt="item.name" />
+            </div>
+
+          </div>
+        </div>
         <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
           <h2 class="text-sm title-font text-gray-500 tracking-widest">{{item.categoryId}}</h2>
           <h1 class="text-gray-900 text-3xl title-font font-medium mb-5">{{item.name}}</h1>
@@ -37,6 +45,7 @@
           </div>
         </div>
       </div>
+      <PhotoPreview v-if="preview" :img="activeImg" @close="preview = false" @next="nextImage" @prev="prevImage"/>
     </div>
   </section>
 </template>
@@ -51,7 +60,7 @@ export default {
       item: {
         "id": 1,
         "name": "Buster S 2022 boat cover",
-        "description": "          Lorem ipsum dolor sit amet, consectetur adipisicing elit. At commodi consequatur ex magni nemo non nostrum officiis perferendis perspiciatis praesentium quae quis, quisquam, ratione rerum sint sit veritatis voluptatem? Aut?",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At commodi consequatur ex magni nemo non nostrum officiis perferendis perspiciatis praesentium quae quis, quisquam, ratione rerum sint sit veritatis voluptatem? Aut?",
         "categoryId": 'Buster',
         "tags": [
           {
@@ -62,7 +71,11 @@ export default {
           }
         ],
         "photos": ["https://i.yapx.ru/S9F4K.png",
-          "https://sun9-87.userapi.com/impg/njbUTyspmS4uw6tYZ2_8apIq4q5UpRe1orW5tQ/grVbY0xoK8E.jpg?size=640x640&quality=95&sign=8742a78348a45f2117e938c8261924fd&type=album",],
+          "https://i.yapx.ru/TEZZ1.jpg",
+          "https://i.yapx.ru/TEZZ3.jpg",
+          "https://i.yapx.ru/TEZZ9.jpg",
+          "https://i.yapx.ru/TEZaA.jpg"
+        ],
         "colors": [
           {
             "name": "Dark Gray",
@@ -81,144 +94,47 @@ export default {
       },
       quantity: 1,
       currentColor: "",
+      activeImg: '',
+      preview: false,
     }
   },
+  mounted() {
+    this.activeImg = this.item.photos[0]
+  },
   methods: {
-    addToCart(item) {
-      this.$store.commit('addItemToCart', this.item)
+    addToCart() {
+      let quantity = this.quantity
+      let item = this.item
+      this.$store.commit('addItemToCart', { ...item, quantity })
     },
-    // async getProduct() {
-    //   const res = await this.$axios.$get('/api/product/byId/' + this.$route.params.id)
-    //   this.item = res
-    //   console.log(res)
-    // },
-  }
+    getIndex() {
+      let index = 0
+      for (let i in this.item.photos) {
+        if (this.item.photos[i] === this.activeImg) {
+          index = i
+        }
+      }
+      return index
+    },
+    nextImage() {
+      let i = this.getIndex()
+      if (i < (this.item.photos.length - 1)) {
+        i ++
+        console.log("next",i)
+        this.activeImg = this.item.photos[i]
+      }
+    },
+    prevImage() {
+      let i = this.getIndex()
+      if (i > 0) {
+        i--
+        console.log("prev",i)
+        this.activeImg = this.item.photos[i]
+      }
+    }
+  },
 }
 </script>
 
 <style scoped>
-input[type="radio"] {
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  display: inline-block;
-  width: 20px;
-  height: 20px;
-  padding: 1px;
-  background-clip: content-box;
-  outline: 2px solid lightgrey;
-  cursor: pointer;
-}
-input[type="radio"]:checked {
-  outline: 2px solid black;
-}
-input[type="radio"]:not(:last-child) {
-  margin-right: 10px;
-}
-.products-colors {
-  padding-left: 2px;
-  margin-bottom: 15px;
-}
-.products-colors-title {
-  display: flex;
-  margin-bottom: 5px;
-}
-  .product {
-    margin-bottom: 20px;
-  }
-  .product-container {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 100px;
-    margin-bottom: 100px;
-  }
-  .product-images {
-    margin-right: 30px;
-    position: relative;
-    max-width: 420px;
-    width: 100%;
-  }
-  .image-active {
-    width: 420px;
-    border-radius: 5px;
-  }
-  .image-zoom-container {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    width: 32px;
-    height: 32px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: #f0f0f0;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-  .image-zoom-container  img {
-    width: 20px;
-    height: 20px;
-  }
-  .product-title {
-    margin-bottom: 20px;
-  }
-  .product-price {
-    font-weight: 400;
-    font-size: 1.3rem;
-  }
-  .price-suffix {
-    font-weight: 400;
-    font-size: 0.9rem;
-  }
-  .product-short-desc {
-    margin-top: 20px;
-    margin-bottom: 10px;
-    overflow-wrap: break-word;
-    font-size: 17px;
-    line-height: 25px;
-  }
-  .product-quantity {
-    display: flex;
-    padding-bottom: 50px;
-    margin-bottom: 10px;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.05);
-  }
-  .quantity {
-    padding: 0 0.7rem;
-    border: none;
-    background: #f2f2f2;
-    width: 40px;
-    height: 40px;
-    box-shadow: rgba(0, 0, 0, 0.125) 0px 1px 1px 0px inset;
-    outline: none;
-    margin-right: 15px;
-  }
-  .product-tech-info {
-    display: flex;
-    flex-direction: column;
-  }
-  .product-tech {
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 22px;
-  }
-  .product-link {
-    font-weight: 600;
-    text-decoration: underline;
-  }
-  .product-link:not(:last-of-type):after{
-    content: ",";
-    margin-right: 4px;
-  }
-  .product-images-list {
-    margin-top: 20px;
-    display: grid;
-    grid-column-gap: 10px;
-    grid-row-gap: 10px;
-    grid-template-columns: calc(20% - 8px) calc(20% - 8px) calc(20% - 8px) calc(20% - 8px) calc(20% - 8px);
-  }
-  .product-images-list img {
-    width: 100%;
-    border-radius: 5px;
-  }
 </style>
