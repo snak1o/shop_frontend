125f67fe312bcfe7a8fdd9e39a5c41252a960757
snak1o <snakwcs@gmail.com>
2022-07-30 13:34:39 +0300

    Changed page route to terms

---
+++ pages/terms.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container mx-auto p-5">
+    <h1 class="text-3xl">Terms of use</h1>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
