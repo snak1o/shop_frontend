7c78e5e0b5477cd9e45c0b94811042336e4dddcb
snak1o <snakwcs@gmail.com>
2022-07-30 13:44:50 +0300

    Add carousel component

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="container mx-auto p-5">
-    <h2 class="text-3xl text-gray-900">
-      New products
-    </h2>
-
-    <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-      <ProductCard
-        class="group relative"
-        v-for="product in products"
-        :key="product.name"
-        :product="product"
-      />
+  <section class="container mx-auto p-5">
+    <Carousel />
+    <div>
+      <h2 class="text-3xl text-gray-900">
+        New products
+      </h2>
+      <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <ProductCard
+          class="group relative"
+          v-for="product in products"
+          :key="product.name"
+          :product="product"
+        />
+      </div>
     </div>
-  </div>
+
+  </section>
 </template>
 
 
@@ -40,12 +43,14 @@ export default {
             {
               "name": "Dark Gray",
               "hex": "#4a4848",
-              "id": 1
+              "id": 1,
+              "price": 20
             },
             {
               "name": "Red",
               "hex": "#ff0000",
-              "id": 3
+              "id": 3,
+              "price": -100,
             }
           ],
           "price": 32.26,
