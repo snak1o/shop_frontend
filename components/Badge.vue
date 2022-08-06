f43bf83fdccfb4d4a1c6a4d91328c8542c175afb
snak1o <snakwcs@gmail.com>
2022-08-06 21:21:01 +0300

    Add badge component

---
+++ components/Badge.vue
@@ -0,0 +1,15 @@
+<template>
+  <span class="text-xs font-semibold px-2.5 py-0.5 rounded">
+    <slot/>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "Badge",
+}
+</script>
+
+<style scoped>
+
+</style>
