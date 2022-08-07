834e41395b7e829a63c621dd04bcfa733f27e4f2
snak1o <snakwcs@gmail.com>
2022-08-07 18:32:02 +0300

    Remove unused mounted hook

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,18 +5,3 @@
     <Footer />
   </div>
 </template>
-
-<script>
-export default {
-  mounted() {
-    if (process.browser) {
-      if (localStorage.hasOwnProperty('cart') && JSON.parse(localStorage.getItem('cart')).length !== 0) {
-        this.$store.commit('updateCart', JSON.parse(localStorage.getItem('cart')))
-      }
-    }
-  }
-}
-</script>
-<style>
-
-</style>
