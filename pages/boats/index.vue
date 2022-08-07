9fec9f21a879aa32582915117dad0ce74b72401e
snak1o <snakwcs@gmail.com>
2022-08-07 18:38:24 +0300

    Add fetching new products

--- pages/boats/index.vue
+++ pages/boats/index.vue
@@ -16,105 +16,12 @@
 
 <script>
 export default {
-  mounted() {
+  async mounted() {
+    this.products = await this.$axios.$get('/items/new')
   },
   data() {
     return {
-      products: [
-        {
-          "id": 1,
-          "name": "Buster S 2022 boat cover",
-          "description": "описание для новой лодочки",
-          "categoryId": 1,
-          "tags": [
-            {
-              "name": "tag1"
-            },
-            {
-              "name": "tag2"
-            }
-          ],
-          "photos": ["https://i.yapx.cc/S9FfL.png",
-            "https://sun9-87.userapi.com/impg/njbUTyspmS4uw6tYZ2_8apIq4q5UpRe1orW5tQ/grVbY0xoK8E.jpg?size=640x640&quality=95&sign=8742a78348a45f2117e938c8261924fd&type=album",],
-          "colors": [
-            {
-              "name": "Dark Gray",
-              "hex": "#4a4848",
-              "id": 1
-            },
-            {
-              "name": "Red",
-              "hex": "#ff0000",
-              "id": 3
-            }
-          ],
-          "price": 32.26,
-          "pricealv": 250,
-          "sku": "F23"
-        },
-        {
-          "id": 1,
-          "name": "Buster XS Pro boat cover",
-          "description": "описание для новой лодочки",
-          "categoryId": 1,
-          "tags": [
-            {
-              "name": "tag1"
-            },
-            {
-              "name": "tag2"
-            }
-          ],
-          "photos": ["https://i.yapx.ru/S9F2Y.png",
-            "https://sun9-87.userapi.com/impg/njbUTyspmS4uw6tYZ2_8apIq4q5UpRe1orW5tQ/grVbY0xoK8E.jpg?size=640x640&quality=95&sign=8742a78348a45f2117e938c8261924fd&type=album",],
-          "colors": [
-            {
-              "name": "Dark Gray",
-              "hex": "#4a4848",
-              "id": 1
-            },
-            {
-              "name": "Red",
-              "hex": "#ff0000",
-              "id": 3
-            }
-          ],
-          "price": 32.26,
-          "pricealv": 320,
-          "sku": "F23"
-        },
-        {"id": 1,
-          "name": "новая лодочка",
-          "description": "описание для новой лодочки",
-          "categoryId": 1,
-          "tags": [
-            {
-              "name": "tag1"
-            },
-            {
-              "name": "tag2"
-            }
-          ],
-          "photos": ["https://i.yapx.ru/S9F4N.png",
-            "https://sun9-87.userapi.com/impg/njbUTyspmS4uw6tYZ2_8apIq4q5UpRe1orW5tQ/grVbY0xoK8E.jpg?size=640x640&quality=95&sign=8742a78348a45f2117e938c8261924fd&type=album",],
-          "colors": [
-            {
-              "name": "Dark Gray",
-              "hex": "#4a4848",
-              "id": 1
-            },
-            {
-              "name": "Red",
-              "hex": "#ff0000",
-              "id": 3
-            }
-          ],
-          "price": 32.26,
-          "pricealv": 40,
-          "sku": "F23"
-        },
-      ],
-      loading: false,
+      products: [],
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="container mx-auto p-5">
+  <section class="container mx-auto p-5 h-full">
     <Carousel />
-    <div>
+    <div class="mt-10">
       <h2 class="text-3xl text-gray-900">
         New products
       </h2>
@@ -14,111 +14,18 @@
         />
       </div>
     </div>
-
   </section>
 </template>
 
 
 <script>
 export default {
+  async mounted() {
+    this.products = await this.$axios.$get('/items/new')
+  },
   data() {
     return {
-      products: [
-        {
-          "id": 1,
-          "name": "Buster S 2022 boat cover",
-          "description": "описание для новой лодочки",
-          "categoryId": 1,
-          "tags": [
-            {
-              "name": "tag1"
-            },
-            {
-              "name": "tag2"
-            }
-          ],
-          "photos": ["https://i.yapx.cc/S9FfL.png",
-            "https://sun9-87.userapi.com/impg/njbUTyspmS4uw6tYZ2_8apIq4q5UpRe1orW5tQ/grVbY0xoK8E.jpg?size=640x640&quality=95&sign=8742a78348a45f2117e938c8261924fd&type=album",],
-          "colors": [
-            {
-              "name": "Dark Gray",
-              "hex": "#4a4848",
-              "id": 1,
-              "price": 20
-            },
-            {
-              "name": "Red",
-              "hex": "#ff0000",
-              "id": 3,
-              "price": -100,
-            }
-          ],
-          "price": 32.26,
-          "pricealv": 250,
-          "sku": "F23"
-        },
-        {
-          "id": 1,
-          "name": "Buster XS Pro boat cover",
-          "description": "описание для новой лодочки",
-          "categoryId": 1,
-          "tags": [
-            {
-              "name": "tag1"
-            },
-            {
-              "name": "tag2"
-            }
-          ],
-          "photos": ["https://i.yapx.ru/S9F2Y.png",
-            "https://sun9-87.userapi.com/impg/njbUTyspmS4uw6tYZ2_8apIq4q5UpRe1orW5tQ/grVbY0xoK8E.jpg?size=640x640&quality=95&sign=8742a78348a45f2117e938c8261924fd&type=album",],
-          "colors": [
-            {
-              "name": "Dark Gray",
-              "hex": "#4a4848",
-              "id": 1
-            },
-            {
-              "name": "Red",
-              "hex": "#ff0000",
-              "id": 3
-            }
-          ],
-          "price": 32.26,
-          "pricealv": 320,
-          "sku": "F23"
-        },
-        {"id": 1,
-          "name": "новая лодочка",
-          "description": "описание для новой лодочки",
-          "categoryId": 1,
-          "tags": [
-            {
-              "name": "tag1"
-            },
-            {
-              "name": "tag2"
-            }
-          ],
-          "photos": ["https://i.yapx.ru/S9F4N.png",
-            "https://sun9-87.userapi.com/impg/njbUTyspmS4uw6tYZ2_8apIq4q5UpRe1orW5tQ/grVbY0xoK8E.jpg?size=640x640&quality=95&sign=8742a78348a45f2117e938c8261924fd&type=album",],
-          "colors": [
-            {
-              "name": "Dark Gray",
-              "hex": "#4a4848",
-              "id": 1
-            },
-            {
-              "name": "Red",
-              "hex": "#ff0000",
-              "id": 3
-            }
-          ],
-          "price": 32.26,
-          "pricealv": 40,
-          "sku": "F23"
-        },
-      ]
+      products: []
     }
   },
 }
