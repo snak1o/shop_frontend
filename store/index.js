3b6b29ca1b19352a360b09bb2d2b375127e4c289
snak1o <snakwcs@gmail.com>
2022-08-07 18:34:14 +0300

    Remove unused function

--- store/index.js
+++ store/index.js
@@ -1,16 +1,15 @@
 export const state = () => ({
   cart: [],
-  cartPrice: 0
+  cartPrice: 0,
 })
 
 export const actions = {
-
 }
 
 export const mutations = {
   removeQuantity(state, item) {
     for (let i = 0; i < Object.keys(state.cart).length; i++) {
-      if (state.cart[i].color === item.color && state.cart[i].id === item.id) {
+      if (state.cart[i].selectedColor.id === item.selectedColor.id && state.cart[i].id === item.id) {
         state.cart[i].quantity--
         localStorage.setItem("cart", JSON.stringify(state.cart))
         return
@@ -21,7 +20,7 @@ export const mutations = {
   },
   addQuantity(state, item) {
     for (let i = 0; i < state.cart.length; i++) {
-      if (state.cart[i].color === item.color && state.cart[i].id === item.id) {
+      if (state.cart[i].selectedColor.id === item.selectedColor.id && state.cart[i].id === item.id) {
         state.cart[i].quantity++
         localStorage.setItem("cart", JSON.stringify(state.cart))
         return
@@ -31,34 +30,25 @@ export const mutations = {
     localStorage.setItem("cart", JSON.stringify(state.cart))
   },
 
-  addItemToCart(state, item) {
-    console.log(item)
-    state.cart.push(item)
-    localStorage.setItem("cart", JSON.stringify(state.cart))
-  },
-
-
   removeItemFromCart(state, item) {
     for (let i = 0; i < Object.keys(state.cart).length; i++) {
-      if (state.cart[i].color === item.color && state.cart[i].id === item.id) {
+      if (state.cart[i].selectedColor.id === item.selectedColor.id && state.cart[i].id === item.id) {
         state.cart.splice(i, 1)
         localStorage.setItem("cart", JSON.stringify(state.cart))
         return
       }
     }
-    console.log(state.cart)
     localStorage.setItem("cart", JSON.stringify(state.cart))
   },
   setCartPrice(state, price) {
-    state.cartPrice += price
+    state.cartPrice = price
   },
   updateCart(state, cart) {
-    console.log("updated")
     state.cart = cart
-  }
+  },
 }
 export const getters = {
   cart: state => state.cart,
-  cartPrice: state => state.cartPrice
+  cartPrice: state => state.cartPrice,
 }
 
