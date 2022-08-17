2afe10176b37b8cda4a87c42420be2b949deaf07
snak1o <snakwcs@gmail.com>
2022-08-18 01:38:25 +0300

    Changed auth plugin to work with any requests needed authentication

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,22 +1,23 @@
 export default function ({ $axios, app }) {
-   let retry = false
-   $axios.onError(error => {
-     if (error.response.status === 401 && retry === false) {
-       retry = true
-       $axios.post('/users/refresh', {}, {
-         withCredentials: true,
-       }).then((res) => {
-         app.$auth.strategy.token.set(res.data.accessToken)
-         app.$auth.fetchUser().then(r => {
-           console.log(r, "fetch")
-         }).catch((e) => {
-           console.log('me', e)
+   $axios.onResponseError(async (error) => {
+     const originalRequest = error.config
+     if (error.config && error.response.status === 401 && !originalRequest._isRetry) {
+       originalRequest._isRetry = true
+       try {
+         const res = await $axios.post('/users/refresh', {}, {
+           withCredentials: true
          })
-       }).catch((e) => {
-         if (e.response.status && e.response.status === 401) {
-           app.$auth.reset()
-         }
-       })
+         await app.$auth.strategy.token.set(res.data.accessToken)
+         return $axios.request(originalRequest)
+       }catch (e) {
+          if (e.response.status === 404) {
+            app.$auth.reset()
+          }
+         throw error
+       }
+     }
+     if (error.response.status === 403) {
+       throw error
      }
    })
   return true
