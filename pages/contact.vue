7c8afd7a32f07c52e69bf239b78fe226089afab3
snak1o <snakwcs@gmail.com>
2022-08-18 18:58:54 +0300

    Add contact and faq empty page

---
+++ pages/contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container mx-auto p-5">
+    <h1 class="text-3xl">Contact us</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "contact"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/faq.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container mx-auto p-5">
+    <h1 class="text-3xl">
+      Frequently asked questions
+    </h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "faq"
+}
+</script>
+
+<style scoped>
+
+</style>
