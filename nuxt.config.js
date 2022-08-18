8d8bcd1483ade58a9e8a2dd3ec2f4d618ac95f0c
snak1o <snakwcs@gmail.com>
2022-08-18 18:02:49 +0300

    Change baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/auth.js'],
   axios: {
+    baseURL: process.env.HOST_API + '/api/v1',
   },
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
