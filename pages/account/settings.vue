0dbf88f21ac7f5a894f80024c73cef01559f2d27
snak1o <snakwcs@gmail.com>
2022-08-22 00:12:04 +0300

    Add notifications Add validations

--- pages/account/settings.vue
+++ pages/account/settings.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="flex flex-col space-y-4">
+    <StaticNotification class="bg-indigo-500" v-if="message" :allowClose="true" @close="message = null">{{message}}</StaticNotification>
+    <StaticNotification class="bg-red-500" v-if="error" :allowClose="true" @close="error = null">{{error}}</StaticNotification>
     <div class="flex flex-col space-y-1">
       <label for="firstName" class="text-gray-600">Change email</label>
       <div class="flex w-full space-y-5 flex-col">
-        <input type="text" class="bg-gray-50 rounded-md border-gray-100 ring-2 ring-gray-100 focus:ring-indigo-500 py-2 px-4 outline-none" v-model="email" id="firstName" />
-        <button class="bg-indigo-500 hover:bg-indigo-600 p-3 text-white rounded-md w-full lg:w-1/4" @click="updateEmail">Update email</button>
+        <input type="text" class="bg-gray-50 rounded-md border-gray-100 ring-2 ring-gray-100 focus:ring-indigo-500 py-2 px-4 outline-none" v-model="getEmail.email" id="firstName" />
+        <button class="bg-indigo-500 hover:bg-indigo-600 p-3 text-white rounded-md w-full lg:w-1/4 disabled:bg-gray-300" @click="updateEmail" :disabled="emailCheck">Update email</button>
       </div>
 
     </div>
@@ -29,32 +31,115 @@ export default {
   name: "Settings",
   data() {
     return {
-      email: '',
+      user: {
+        email: ""
+      },
       oldPassword: "",
       newPassword: "",
-      retypeNewPassword: ""
+      retypeNewPassword: "",
+      message: null,
+      error: null,
     }
   },
-  async mounted() {
-    this.email = this.$auth.user.email
+  computed: {
+    getEmail() {
+      this.user.email = this.$auth.user.email
+      return this.user
+    },
+    emailCheck() {
+      return (this.user.email === this.$auth.user.email)
+    }
   },
   methods: {
     async updateEmail() {
-      await this.$axios.$put('/users/email', {
-        email: this.email
-      })
+      if (this.emailValidation()) {
+        try {
+          const res = await this.$axios.put('/users/email', {
+            email: this.user.email.trim()
+          })
+          if (res && res.status === 200) {
+            this.message = "Email updated successfully."
+            this.$auth.setUser({
+              email: this.user.email.trim()
+            })
+          }
+        } catch (e) {
+          if (e.response) {
+            if (e.response.status === 409) {
+              this.error = "This email address is already in use."
+            }
+            else {
+              this.error = `Error ${e.response.status}.`
+            }
+          }
+          else {
+            this.error = `Error ${e.message}.`
+          }
+        }
+
+      }
     },
     async updatePassword() {
-      await this.$axios.$put('/users/password', {
-        newPassword: this.newPassword,
-        oldPassword: this.oldPassword
-      })
+      if (this.passwordValidation()) {
+        try {
+          const res = await this.$axios.put('/users/password', {
+            newPassword: this.newPassword.trim(),
+            oldPassword: this.oldPassword.trim()
+          })
+          if (res && res.status === 200) {
+            this.message = "Password updated successfully."
+          }
+        }catch (e) {
+          if (e.response) {
+            if (e.response.status === 409) {
+              this.error = "Wrong old password."
+            }
+            else {
+              this.error = `Error ${e.response.status}.`
+            }
+          }
+          else {
+            this.error = `Error ${e.message}.`
+          }
+        }
+      }
+      this.oldPassword = this.newPassword = this.retypeNewPassword = ""
     },
     async removeAccount() {
       //validation + remove refresh token
-      const res = await this.$axios.delete('/users/me')
+      const res = await this.$axios.delete('/users/me', {
+        withCredentials: true
+      })
       if (res && res.status === 200) {
         await this.$auth.reset()
+      }
+    },
+    emailValidation() {
+      if (!this.user.email.trim().match( /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+        this.error = "Email format must be example@example.com"
+        return false
+      }
+      else{
+        this.error = null
+        return true
+      }
+    },
+    passwordValidation() {
+      if (this.oldPassword.trim().length < 1) {
+        this.error = "Old password can not be empty"
+        return false
+      }
+      if (this.newPassword.trim().length < 8) {
+        this.error = "New password length can not be less than 8."
+        return false
+      }
+      if (this.newPassword.trim() !== this.retypeNewPassword.trim()) {
+        this.error = "Password do not match."
+        return false
+      }
+      else {
+        this.error = null
+        return true
       }
     }
   }
