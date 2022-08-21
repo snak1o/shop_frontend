94d2ecac18e6c1bdff207ecbcfa4e6c52abdbcc7
snak1o <snakwcs@gmail.com>
2022-08-21 17:17:34 +0300

    Change header

--- pages/account.vue
+++ pages/account.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container mx-auto p-5 md:w-screen">
-    <h1 class="text-3xl">Welcome back, {{$auth.user.login}}</h1>
+    <h1 class="text-3xl">Account</h1>
     <div class="flex flex-col items-center lg:items-start space-y-10 lg:space-y-0 lg:flex-row lg:space-x-10 mt-5">
       <AccountSidebar class="w-full lg:w-1/5" @logout="logout"/>
       <NuxtChild class="w-full lg:w-4/5" />
