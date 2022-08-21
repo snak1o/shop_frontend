634c202e8c9e46bad2b103b85db59ed9ce3b2b5c
snak1o <snakwcs@gmail.com>
2022-08-21 17:18:01 +0300

    Add validation if error have response

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,23 +1,25 @@
 export default function ({ $axios, app }) {
    $axios.onResponseError(async (error) => {
      const originalRequest = error.config
-     if (error.config && error.response.status === 401 && !originalRequest._isRetry) {
-       originalRequest._isRetry = true
-       try {
-         const res = await $axios.post('/users/refresh', {}, {
-           withCredentials: true
-         })
-         await app.$auth.strategy.token.set(res.data.accessToken)
-         return $axios.request(originalRequest)
-       }catch (e) {
-          if (e.response.status === 404) {
-            app.$auth.reset()
-          }
+     if (error.response) {
+       if (error.config && error.response.status === 401 && !originalRequest._isRetry) {
+         originalRequest._isRetry = true
+         try {
+           const res = await $axios.post('/users/refresh', {}, {
+             withCredentials: true
+           })
+           await app.$auth.strategy.token.set(res.data.accessToken)
+           return $axios.request(originalRequest)
+         }catch (e) {
+           if (e.response.status === 404) {
+             app.$auth.reset()
+           }
+           throw error
+         }
+       }
+       if (error.response.status === 403) {
          throw error
        }
-     }
-     if (error.response.status === 403) {
-       throw error
      }
    })
   return true
