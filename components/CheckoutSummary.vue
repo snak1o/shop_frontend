50fe0b622b3eaf3bee831583db809aef73b07618
snak1o <snakwcs@gmail.com>
2022-10-01 13:03:00 +0300

    Update checkout component

--- components/CheckoutSummary.vue
+++ components/CheckoutSummary.vue
@@ -1,13 +1,25 @@
 <template>
-<div class="bg-gray-50 p-10 rounded-md" v-if="getPrice && selectedShipping">
-  <h2 class="text-2xl font-semibold select-none">Order summary</h2>
+<div class="bg-gray-50 px-10 py-6 rounded-md" v-if="getPrice && selectedShipping">
+  <h2 class="text-2xl font-semibold select-none">Order information</h2>
   <ul>
-    <li class="flex justify-between border-b py-4"><span class="text-gray-500">Subtotal</span><span class="font-semibold">{{(getPrice - (getPrice * 0.1935)).toFixed(2)}}€</span></li>
-    <li class="flex justify-between border-b py-4"><span class="text-gray-500">Shipping cost</span><span class="font-semibold">{{selectedShipping.price === 0 ? 'free': (selectedShipping.price - (selectedShipping.price * 0.1935)).toFixed(2) + '€'}}</span></li>
-    <li class="flex justify-between border-b py-4"><span class="text-gray-500">Tax fee</span><span class="font-semibold">{{((getPrice * 0.1935) + (selectedShipping.price * 0.1935)).toFixed(2)}}€</span></li>
-    <li class="flex justify-between py-4 text-xl"><span>Order total</span><span>{{(getSum + selectedShipping.price).toFixed(2)}}€</span></li>
+    <li class="flex flex-col border-b py-4">
+      <div class="flex justify-between" v-for="product in products">
+        <span class="text-gray-500">{{product.name}} ({{product.color.name}}) <strong>x{{product.quantity}}</strong></span><span class="font-semibold">{{(product.price + product.color.price).toFixed(2)}}€</span>
+      </div>
+    </li>
+    <li class="flex justify-between border-b py-4"><span class="text-gray-500">Shipping ({{selectedShipping.companyName}})</span><span class="font-semibold">{{selectedShipping.price.toFixed(2)}}€</span></li>
+    <li class="flex justify-between py-4 text-xl items-center">
+      <span>Order total</span>
+      <div class="flex flex-col">
+        <span class="flex justify-end">
+          {{(getPrice).toFixed(2)}}€
+        </span>
+        <span class="text-sm text-gray-500">
+          (include tax {{(getPrice * 0.1935).toFixed(2)}}€)
+        </span>
+      </div>
+    </li>
   </ul>
-  <button class="py-3 mt-3 w-full bg-indigo-500 hover:bg-indigo-600 text-white font-semibold rounded-md">Checkout</button>
 </div>
 </template>
 
@@ -17,7 +29,8 @@ export default {
   data() {
     return {
       getPrice: this.$store.getters['cartPrice'],
-      selectedShipping: this.$store.getters['shipping']
+      selectedShipping: this.$store.getters['shipping'],
+      products: this.$store.getters['cartProducts']
     }
   },
 }
